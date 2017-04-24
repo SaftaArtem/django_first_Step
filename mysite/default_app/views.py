5eb2160071ba231e23035111bf4002b817691d8e
Artem Safta <Artem Safta>
2017-04-24 19:46:54 +0300

    10

--- mysite/default_app/views.py
+++ mysite/default_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from catalog.models import Catalog 
 
 
 def home(request):
     context = {
     	'title': 'home page title',
+    	'catalog': Catalog.objects.all(),
     }
     return render(request, 'default_app/home.html', context)
