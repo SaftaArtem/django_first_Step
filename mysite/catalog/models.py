7d7bc7b072315793481d7040dce7afc0c54ab439
Artem Safta <Artem Safta>
2017-04-24 19:00:42 +0300

    8

--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Catalog(models.Model):
+	title = models.CharField(max_length=150)
+	discription = models.TextField()
+
+	def __str__(self):
+		return self.title
