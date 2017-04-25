a545b506d2bf9d895d8518f7c2956a7c6f8ebb73
Artem Safta <Artem Safta>
2017-04-25 08:22:53 +0300

    11

--- mysite/default_app/views.py
+++ mysite/default_app/views.py
@@ -4,7 +4,7 @@ from catalog.models import Catalog
 
 def home(request):
     context = {
-    	'title': 'home page title',
+    	'page_header': 'home page title',
     	'catalog': Catalog.objects.all(),
     }
     return render(request, 'default_app/home.html', context)
