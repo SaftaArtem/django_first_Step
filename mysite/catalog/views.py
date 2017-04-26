7e53d0dafa8ccdd485ec9c9c2fdc7743e4778524
Artem Safta <Artem Safta>
2017-04-26 09:37:54 +0300

    15

--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Catalog
+#from django.http import HttpResponse
 
 def catalog_home(request):
-	return HttpResponse('Catalog')
+	context = {
+	'page_header': 'catalog page title',
+    'catalog': Catalog.objects.all(),
+	}
+	return render(request, 'catalog/catalog.html', context)
+	#return HttpResponse('Catalog')
 
 def catalog_detail(request, catalog_id):
-	return HttpResponse('Catalog detail page' + catalog_id)
+	catalog_item = get_object_or_404(Catalog, pk = catalog_id)
+	context = {
+	'page_header': catalog_item.title ,
+    'catalog_item': catalog_item,
+	}
+	return render(request, 'catalog/catalog_detail.html', context)
+	#return HttpResponse('Catalog detail page' + catalog_id)
