b7019d711076f94fc452c1858acd4a1ccde11824
Artem Safta <Artem Safta>
2017-04-27 08:28:56 +0300

    16

--- mysite/catalog/urls.py
+++ mysite/catalog/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'catalog'
 urlpatterns = [
-	url(r'^$', views.catalog_home, name="catalog_home"),
+	url(r'^$', views.catalog_home, name="home"),
 	#url(r'^(?P<catalog_id>[0-9]+)$', views.catalog_detail, name='catalog_detail'),
-	url(r'^(?P<catalog_id>[\d+]+)$', views.catalog_detail, name='catalog_detail'),
+	url(r'^(?P<catalog_id>[\d+]+)$', views.catalog_detail, name='detail'),
 ]
