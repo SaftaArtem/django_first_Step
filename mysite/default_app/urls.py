5ef5757bb50d63921e94b59db18b2618606805e7
Artem Safta <Artem Safta>
2017-04-28 09:40:44 +0300

    20'

--- mysite/default_app/urls.py
+++ mysite/default_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^contacts$', views.contacts, name='contacts'),
 ]
--- mysite/default_app/views.py
+++ mysite/default_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Catalog 
+from django.http import HttpResponse
 
 
 def home(request):
@@ -8,3 +9,19 @@ def home(request):
     	'catalog': Catalog.objects.all(),
     }
     return render(request, 'default_app/home.html', context)
+
+def contacts(request):
+	contacts_form_data = {}
+	
+	if request.method == 'POST':
+		#contacts_form_data = request.POST
+		user_name = request.POST.get('user_name')
+		user_email = request.POST.get('user_email')
+		message = request.POST.get('message')
+
+		contacts_form_data.update({'user_name': user_name, 'user_email': user_email, 'message': message})
+	context = {
+		'page_header':'Contacts form',
+		'contacts_form_data': contacts_form_data,
+	}
+	return render(request, 'default_app/contacts_form.html', context)
